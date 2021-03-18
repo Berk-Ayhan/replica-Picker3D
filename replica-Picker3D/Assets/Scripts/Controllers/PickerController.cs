6bda778807cc61eda5a043d69db7619e596f9795
Berk-Ayhan <berkayhan3443@gmail.com>
2021-03-19 00:04:53 +0300

    Adding picker inputs

--- replica-Picker3D/Assets/Scripts/Controllers/PickerController.cs
+++ replica-Picker3D/Assets/Scripts/Controllers/PickerController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PickerController : MonoBehaviour
 {
+    Rigidbody _rb;
     Mover _mover;
+
+    Vector3 first, second;
+    public float speed = 2;
     void Awake()
     {
+        _rb = GetComponent<Rigidbody>();
         _mover = GetComponent<Mover>();
     }
-
-    // Update is called once per frame
     void FixedUpdate()
     {
         _mover.Move();
+        if (Input.GetMouseButtonDown(0))
+        {
+            first = Camera.main.ScreenToViewportPoint(new Vector3(Input.mousePosition.x, 0, 0));
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            second = Camera.main.ScreenToViewportPoint(new Vector3(Input.mousePosition.x, 0, 0));
+            Vector3 diff = second - first;
+            transform.position += diff * speed;
+            first = second;
+        }
     }
 }
